a3d7341e6c78422e3a0336fefb02747df1743065
Reinette <reinettetinekerr@gmail.com>
2023-03-08 22:03:11 +0800

    adding headers4

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -4,12 +4,12 @@ const docs = require('@googleapis/docs')
 export async function GET(request: Request) {
   const headers = request.headers;
   
-  // const auth = new docs.auth.GoogleAuth({ keyFilename: './secret.json', scopes: ['https://www.googleapis.com/auth/documents.readonly'] })
-  // const authClient = await auth.getClient();
-  // const client = await docs.docs({
-  //   version: 'v1',
-  //   auth: authClient
-  // });
+  const auth = new docs.auth.GoogleAuth({ keyFilename: './secret.json', scopes: ['https://www.googleapis.com/auth/documents.readonly'] })
+  const authClient = await auth.getClient();
+  const client = await docs.docs({
+    version: 'v1',
+    auth: authClient
+  });
   // const res = await client.documents.get({ documentId: '1EFCR_D8mosNVDUBMYhc-2Q8srI758kXvjF-yIZm-OP0' })
   // const { data } = res;
   // const doc = data.body.content.reduce((acc:any, cur:any) => {
